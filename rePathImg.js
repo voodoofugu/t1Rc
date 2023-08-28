8c7772430245ed934903032a27c97ec10b53903a
Voodoofugu <voodoofugu@gmail.com>
2023-08-28 14:15:52 +0300

    fix

--- rePathImg.js
+++ rePathImg.js
@@ -3,28 +3,28 @@ const glob = require("glob");
 
 // Функция для обновления путей в файле
 function updatePathsInFile(filePath, imgPathAlias) {
-  const content = fs.readFileSync(filePath, "utf8");
-  const updatedContent = content.replace(
-    /((?:background(?:-image)?|cursor|content):\s*[^;]*url\([]?)(?!~)(.+?\.(png|jpg|jpeg|gif|svg))['"]?\)/g,
-    // /(background(?:-image)?:\s*[^;]*url\(['"]?)([^)'"]+\.(png|jpg|jpeg|gif|svg))['"]?\)/g,
-    (match, prefix, imagePath, extension) => {
-      // Удаляем одинарные и двойные кавычки из пути к изображению
-      const cleanImagePath = imagePath.replace(/['"]/g, "");
-      return `${prefix}~${imgPathAlias}/${cleanImagePath})`;
-    }
-  );
+    const content = fs.readFileSync(filePath, "utf8");
+    const updatedContent = content.replace(
+        /((?:background(?:-image)?|cursor|content):\s*[^;]*url\([]?)(?!~)(.+?\.(png|jpg|jpeg|gif|svg))['"]?\)/g,
+        // /(background(?:-image)?:\s*[^;]*url\(['"]?)([^)'"]+\.(png|jpg|jpeg|gif|svg))['"]?\)/g,
+        (match, prefix, imagePath, extension) => {
+            // Удаляем одинарные и двойные кавычки из пути к изображению
+            const cleanImagePath = imagePath.replace(/['"]/g, "");
+            return `${prefix}~${imgPathAlias}/${cleanImagePath})`;
+        }
+    );
 
-  fs.writeFileSync(filePath, updatedContent, "utf8");
-  console.log(`Updated file: ${filePath}`);
+    fs.writeFileSync(filePath, updatedContent, "utf8");
+    console.log(`Updated file: ${filePath}`);
 }
 
 // Основная функция для обновления путей во всех SCSS файлах в папке src/styles
 function updateImagePaths(imgPathAlias) {
-  const scssFiles = glob.sync("src/styles/**/*.scss");
+    const scssFiles = glob.sync("src/styles/**/*.scss");
 
-  scssFiles.forEach((filePath) => {
-    updatePathsInFile(filePath, imgPathAlias);
-  });
+    scssFiles.forEach((filePath) => {
+        updatePathsInFile(filePath, imgPathAlias);
+    });
 }
 
 // Заменить 'imgPath' на ваш алиас
