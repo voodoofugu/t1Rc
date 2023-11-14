32b845a45e88ede494ca5c4082c3e3e2b12209fd
Voodoofugu <voodoofugu@gmail.com>
2023-11-14 19:30:39 +0300

    fix

--- src/components/TooltipProvider.jsx
+++ src/components/TooltipProvider.jsx
@@ -1,10 +1,13 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { createPortal } from "react-dom";
+import Stylus from "../img/template/svg/Stylus";
 
 export default function TooltipProvider({ children, text }) {
   const refSetTimeout = useRef(null);
   const tooltipRef = useRef(null);
   const [visibleClass, setVisibleClass] = useState("");
+  const [positionClassY, setPositionClassY] = useState("");
+  const [positionClassX, setPositionClassX] = useState("");
   const [showToolTip, setShowToolTip] = useState(false);
   const [tooltipData, setTooltipData] = useState({
     left: 0,
@@ -13,67 +16,68 @@ export default function TooltipProvider({ children, text }) {
     tooltipHeight: 0,
   });
 
-  useEffect(() => {
-    if (showToolTip) {
-      updateTooltipData();
-    }
-    return () => {
-      if (refSetTimeout.current) {
-        clearTimeout(refSetTimeout.current);
-      }
-    };
-  }, [showToolTip, tooltipRef.current]);
-
-  async function updateTooltipData() {
+  function updateTooltipData() {
     if (tooltipRef.current) {
-      const tooltipWidth = tooltipRef.current.offsetWidth;
-      const tooltipHeight = tooltipRef.current.offsetHeight;
-
-      // Обновляем данные
       setTooltipData((prevData) => ({
         ...prevData,
-        tooltipWidth,
-        tooltipHeight,
+        tooltipWidth: tooltipRef.current.offsetWidth,
+        tooltipHeight: tooltipRef.current.offsetHeight,
       }));
-
-      // Ждем рендера после обновления данных
-      await new Promise((resolve) => setTimeout(resolve, 0));
-
-      setVisibleClass("visible");
     }
   }
 
-  function calculateTooltipPosition({ left, top, height, width }) {
-    const { innerHeight } = window;
-    const halfHeight = innerHeight / 2;
+  function addNewData(event) {
+    setTooltipData((prevData) => {
+      const { left, top, height, width } = event.target.getBoundingClientRect();
+      const infoPageBoxCenterY = top + height / 2;
+      const infoPageBoxCenterX = left + width / 2;
 
-    let newTooltipData = { left: 0, top: 0 };
+      const { innerHeight, innerWidth } = window;
+      const halfHeight = innerHeight / 2;
+      const oneFreethWidth = innerWidth / 3;
 
-    if (top < halfHeight) {
-      newTooltipData.top = top + height + 16;
-    } else {
-      newTooltipData.top = top - tooltipData.tooltipHeight - 16;
-    }
+      let newTooltipData = { ...prevData };
 
-    newTooltipData.left = left - tooltipData.tooltipWidth / 2 + width / 2;
+      if (infoPageBoxCenterY < halfHeight) {
+        newTooltipData.top = top + height + 16;
+        setPositionClassY("top");
+      } else {
+        newTooltipData.top = top - prevData.tooltipHeight - 16;
+        setPositionClassY("bottom");
+      }
 
-    return newTooltipData;
+      newTooltipData.left = left - prevData.tooltipWidth / 2 + width / 2;
+
+      if (infoPageBoxCenterX < oneFreethWidth) {
+        newTooltipData.left = left - width;
+        setPositionClassX("left");
+      }
+      if (infoPageBoxCenterX > oneFreethWidth * 2) {
+        newTooltipData.left = left - prevData.tooltipWidth + width * 2;
+        setPositionClassX("right");
+      }
+
+      return newTooltipData;
+    });
   }
 
   function onMouseEnterHandler(event) {
-    const boundingRect = event.target.getBoundingClientRect();
-
     if (refSetTimeout.current) {
       clearTimeout(refSetTimeout.current);
     }
 
     refSetTimeout.current = setTimeout(() => {
-      // Сначала обновляем данные тултипа
-      setTooltipData(calculateTooltipPosition(boundingRect));
-
-      // Затем устанавливаем видимость
       setShowToolTip(true);
-    }, 300);
+      refSetTimeout.current = setTimeout(() => {
+        updateTooltipData();
+      }, 50);
+      refSetTimeout.current = setTimeout(() => {
+        addNewData(event);
+      }, 100);
+      refSetTimeout.current = setTimeout(() => {
+        setVisibleClass("visible");
+      }, 350);
+    }, 200);
   }
 
   function onMouseLeaveHandler() {
@@ -94,10 +98,13 @@ export default function TooltipProvider({ children, text }) {
       {showToolTip &&
         createPortal(
           <div
-            className={`tooltipTemplate ${visibleClass}`}
+            className={`tooltipTemplate ${visibleClass} ${positionClassY} ${positionClassX}`}
             style={tooltipData}
             ref={tooltipRef}
           >
+            <div className="icn">
+              <Stylus />
+            </div>
             {text}
           </div>,
           document.querySelector("#root > div > main")
