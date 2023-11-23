297a4dce17210d9257f0b7d55659413e2129c184
Voodoofugu <voodoofugu@gmail.com>
2023-11-23 17:20:41 +0300

    fix

--- src/stylesMap.jsx
+++
@@ -1,4 +0,0 @@
-import "../node_modules/normalize.css/normalize.css";
-import "./styles/scss/01-all.scss";
-import "./styles/scss/v2-screen-main.scss";
-import "./styles/scss/v2-screen-main-dark-world.scss";
