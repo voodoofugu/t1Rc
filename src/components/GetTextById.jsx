e19c4afc959f1d8769000750df6ea953ac17afa0
Voodoofugu <voodoofugu@gmail.com>
2023-11-23 19:54:58 +0300

    project fix

--- src/components/GetTextById.jsx
+++
@@ -1,9 +0,0 @@
-import AaComponMap from "../components/AaComponMap.jsx";
-
-const GetTextById = (id) => {
-    // Предположим, что у вас есть TextArray как в предыдущем коде
-    const textItem = AaComponMap.TextMap.find((item) => item.id === id);
-    return textItem ? textItem.text : "";
-};
-
-export default GetTextById;
--- src/components/HelmetComponent.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Helmet } from "react-helmet-async";
-
-export default function HelmetComponent({ children }) {
-  return <Helmet>{children}</Helmet>;
-}
