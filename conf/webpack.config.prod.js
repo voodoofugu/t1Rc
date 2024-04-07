fe5487c0ba491c0d1a06b6530761b02566efd159
Voodoofugu <voodoofugu@gmail.com>
2024-04-08 01:05:38 +0300

    project fix

--- conf/webpack.config.prod.js
+++ conf/webpack.config.prod.js
@@ -1,54 +1,44 @@
 import { merge } from "webpack-merge";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import webpackConfig from "./webpack.config";
-import fs from "fs";
+import webpackConfig from "./webpack.config.js";
 import path from "path";
+import { EsbuildPlugin } from "esbuild-loader";
 
-// Функция для динамической генерации HtmlWebpackPlugin
 function generateHtmlPlugins() {
-  const pageFiles = fs.readdirSync("./titans_rc/pages");
-
-  return pageFiles
-    .map((file) => {
-      const pageName = path.parse(file).name;
-
-      if (file === "template.html") {
-        return null; // Не создавать HtmlWebpackPlugin для template.html
-      } else {
-        return new HtmlWebpackPlugin({
-          template: path.resolve("titans_rc", "htmlContent.ejs"),
-          filename: `pages/${pageName}.html`,
-          minify: true,
-          inject: false, // не забудь проверить inject
-          excludeChunks: ["template"],
-          title: pageName,
-          pageName: pageName,
-          scriptLoading: "defer",
-          // sourceMap: true,
-        });
-      }
-    })
-    .filter((plugin) => plugin !== null); // Удалить null значения из массива
+  return new HtmlWebpackPlugin({
+    template: path.resolve("titans_rc", "htmlContent.ejs"),
+    filename: "index.html",
+    title: "Template",
+    minify: true, // Минифицируем HTML для продакшена
+  });
 }
 
-const plugins = [...generateHtmlPlugins()];
-
-const webpackConfigProd = merge(webpackConfig, {
+const prodConfig = merge(webpackConfig, {
   mode: "production",
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
-    minimize: true,
-  },
   output: {
-    path: path.resolve("dist"),
-    clean: true,
-    filename: "scripts/[name].[contenthash].js",
+    filename: "scripts/[name].[contenthash].js", // Используем хэшированные имена файлов для кэширования
     chunkFilename: "scripts/[name].[contenthash].js",
-    assetModuleFilename: "[name][ext]",
-    publicPath: "../",
+    assetModuleFilename: "assets/[name].[contenthash][ext]",
+    publicPath: "/", // Обновляем publicPath для корректной работы с ресурсами на сервере
   },
-  // devtool: "source-map",
-  plugins: [...plugins],
+  devtool: false, // Отключаем source maps для продакшена
+  plugins: [
+    generateHtmlPlugins(),
+    new EsbuildPlugin({
+      target: "es2015",
+    }),
+    // Другие плагины, которые вам необходимы для продакшена
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // Дополнительные оптимизации, например, минификация и оптимизация JavaScript
+      new EsbuildPlugin({
+        target: "es2015",
+        minify: true,
+      }),
+    ],
+  },
 });
+
+export default prodConfig;
