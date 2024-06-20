d82415351a0b0dbb628d6d3a60549264489c94f6
Voodoofugu <voodoofugu@gmail.com>
2024-06-20 15:10:22 +0300

    dating fix

--- src/components/projetComponents/pagesComponents/Dating.jsx
+++ src/components/projetComponents/pagesComponents/Dating.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { useDispatch } from "../../templateComponents/GlobalStateStor";
-import ItemBox from "../UIComponents/ItemBox";
 
-export const cssFiles = ["dating"];
+import ItemBox from "../UIComponents/ItemBox";
+import ResCount from "../UIComponents/ResCount";
+
+export const cssFiles = [
+  "dating",
+  "screen-superhero",
+  "screen-unipop-subscription",
+];
 
 export default function Dating({ pageName, children }) {
   const dispatch = useDispatch();
@@ -12,6 +18,31 @@ export default function Dating({ pageName, children }) {
       <div className="main-bg dating"></div>
       <div className="dating_content">
         <div className="dating_title">Your date</div>
+        <div className="counterBox">
+          <ResCount
+            img="img/ic-diamond.png"
+            value="234"
+            plus
+            onClick={() => {
+              dispatch({
+                type: "POPUP_OPEN",
+                payload: {
+                  mpopClass: "m-popup essence-buy",
+                  popTit: "Buy Dices",
+                  popCont: [
+                    "BuyShop",
+                    {
+                      img1: "dating/datingCoin_1",
+                      img2: "dating/datingCoin_2",
+                      img3: "dating/datingCoin_3",
+                      img4: "dating/datingCoin_4",
+                    },
+                  ],
+                },
+              });
+            }}
+          />
+        </div>
         <div className="color-btn exit">
           <div className="color-btn-text"></div>
         </div>
