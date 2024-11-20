729c03be82db18ea338050145537719074f6440a
voodoofugu <schilin.georg@gmail.com>
2024-11-20 23:36:18 +0300

    dependencies fix

--- conf/loaders.ts
+++ conf/loaders.ts
@@ -20,7 +20,7 @@ export function loaders(options: BuildOptions): ModuleOptions["rules"] {
   const fileLoader = {
     test: /\.(woff2?|jpe?g|png|webp|gif|svg)$/i,
     use: {
-      loader: "file-loader",
+      loader: "esbuild-loader",
       options: {
         name: "[name][ext]",
         outputPath: "assets",
