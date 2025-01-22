d1e5747e1a4ebdd8c32e5ab3706a668c4a91d473
voodoofugu <schilin.georg@gmail.com>
2025-01-22 15:11:52 +0300

    fortune fix

--- src/components/projetComponents/popupsContetnt/FortunaMainRound16Big.jsx
+++ src/components/projetComponents/popupsContetnt/FortunaMainRound16Big.jsx
@@ -12,7 +12,7 @@ export default function FortunaMainRound16Big() {
 
   return (
     <>
-      <ImageBg className="bG framedBg" img="img/bGs/fortuneBg_hw.jpg" />
+      <ImageBg className="bG framedBg" img="img/bGs/fortuneBg.jpg" />
 
       <FraimedTitle
         className="cornersTop"
@@ -112,7 +112,7 @@ export default function FortunaMainRound16Big() {
 
       <ImageBg
         className="girl"
-        img="img/images/hero-all/tithero-428/icons/break-girl.png"
+        img="img/images/hero-all/tithero-464/icons/break-girl.png"
       />
 
       <div className="fortuna-main-box">
