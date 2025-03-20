44310fd537e5779632995155c0e4875603c45c72
voodoofugu <schilin.georg@gmail.com>
2025-03-20 14:04:11 +0300

    project fix

--- src/components/projetComponents/pagesComponents/Dating.jsx
+++ src/components/projetComponents/pagesComponents/Dating.jsx
@@ -20,20 +20,20 @@ export const cssFiles = [
   "daiting-gifts-pop",
 ];
 
-function multiply(a, b, c) {
-  return a * b * c;
-}
-function curry(fn) { 
-  return function curried(...args) {
-    if (args.length >= fn.length) {
-      return fn.apply(this, args);
-    }
-    return (...nextArgs) => curried(...args, ...nextArgs);
-  };
-}
+// function multiply(a, b, c) {
+//   return a * b * c;
+// }
+// function curry(fn) {
+//   return function curried(...args) {
+//     if (args.length >= fn.length) {
+//       return fn.apply(this, args);
+//     }
+//     return (...nextArgs) => curried(...args, ...nextArgs);
+//   };
+// }
 
-const curriedMultiply = curry(multiply);
-console.log(curriedMultiply(2)(3)(4));
+// const curriedMultiply = curry(multiply);
+// console.log(curriedMultiply(2)(3)(4));
 
 function FakeComponent() {
   return null;
