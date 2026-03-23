9ab541f187f58e20ec5ff5c11072d7aaea8be9c3
voodoofug <voodoofugu@gmail.com>
2026-03-23 15:00:31 +0300

    WorldBtnBox fix

--- src/components/projetComponents/UIComponents/WorldBtnBox.jsx
+++ src/components/projetComponents/UIComponents/WorldBtnBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 
 const inlineStyles = {
   position: "absolute",
@@ -9,53 +9,26 @@ const inlineStyles = {
   width: "140px",
 };
 
-export default function WorldBtnBox({ pageName, onClick }) {
-  const currentPath = window.location.hash.substring(3).split("/")[0];
+export default function WorldBtnBox({ pageName }) {
   const [isDarkWorld, setIsDarkWorld] = useState(false);
+
   const btnClickHandler = () => {
+    const currentPath = window.location.hash.substring(3).split("/")[0];
+
+    if (pageName === currentPath) {
+      const main = document.querySelector(".main");
+      if (!main) return;
+
+      main.classList.toggle("world1", isDarkWorld);
+      main.classList.toggle("world3", !isDarkWorld);
+    }
+
     setIsDarkWorld((prevIsDarkWorld) => !prevIsDarkWorld);
   };
 
-  useEffect(() => {
-    if (pageName === currentPath) {
-      const main = document.querySelector(".main");
-      const reG = document.querySelector(
-        ".resource-panel > div:nth-of-type(2)"
-      );
-      const reI = document.querySelector(".resource-pic > img");
-      const vvT = document.querySelector(".value-vip-ticket");
-
-      const updateUI = () => {
-        main.classList.toggle("world1", !isDarkWorld);
-        main.classList.toggle("world3", isDarkWorld);
-        // reG.classList.toggle("resource-gold", !isDarkWorld);
-        // reG.classList.toggle("resource-dark_gold", isDarkWorld);
-        // reI.src = isDarkWorld ? "rc/v2-res-gold.png" : "img/v2-res-dark.png";
-        // vvT.style.display = isDarkWorld ? "block" : "none";
-      };
-
-      const worldBtn = document.querySelector(".color-btn.green.world-btn");
-      if (worldBtn) {
-        worldBtn.addEventListener("click", btnClickHandler);
-      }
-
-      updateUI();
-
-      return () => {
-        const worldBtn = document.querySelector(".color-btn.green.world-btn");
-        if (worldBtn) {
-          worldBtn.removeEventListener("click", btnClickHandler);
-        }
-      };
-    }
-  }, [isDarkWorld, pageName, currentPath]);
-
   return (
     <div style={inlineStyles}>
-      <div
-        className="color-btn green world-btn"
-        onClick={() => onClick(isDarkWorld)}
-      >
+      <div className="color-btn green world-btn" onClick={btnClickHandler}>
         <div className="color-btn-text">
           {isDarkWorld ? "To Light" : "To Dark"}
         </div>
